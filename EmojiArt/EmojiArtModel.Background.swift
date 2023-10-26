662e8d00131153ec222bb21a599005453eb3ed73
alexandre.f.ext@e-ai.com.br <alexandre.f.ext@e-ai.com.br>
2023-10-26 17:08:51 -0300

    Creating EmojiArt model

---
+++ EmojiArt/EmojiArtModel.Background.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiArtModel.Background.swift
+//  EmojiArt
+//
+//  Created by Alexandre Papanis on 10/26/23.
+//
+
+import Foundation
+
+enum Background {
+    case blank
+    case url(URL)
+    case imageData(Data)
+    
+}
---
+++ EmojiArt/EmojiArtModel.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiArtModel.swift
+//  EmojiArt
+//
+//  Created by Alexandre Papanis on 10/26/23.
+//
+
+import Foundation
+
+struct EmojiArtModel {
+    var background = Background.blank
+    var emojis = [Emoji]()
+    
+    struct Emoji: Identifiable {
+        let text: String
+        let x: Int
+        let y: Int
+        var size: Int
+        let id: Int
+        
+        //prevent other classes to create new Emoji
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+    }
+    
+    init() { }
+    
+    private var uniqueEmojiId = 0
+    mutating func addEmoji(_ text: String, at location: (x: Int, y: Int), size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text: text, x: location.x, y: location.y, size: size, id: uniqueEmojiId))
+    }
+}
