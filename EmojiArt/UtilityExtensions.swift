186f21ed6edc91358131d2def95fa0460ddaa1f2
alexandre.f.ext@e-ai.com.br <alexpapanis@gmail.com>
2023-11-07 12:40:45 -0300

    Drag gesture simultaneously with zoom gesture

--- EmojiArt/UtilityExtensions.swift
+++ EmojiArt/UtilityExtensions.swift
@@ -96,8 +96,25 @@ extension CGPoint {
     static func /(lhs: Self, rhs: CGSize) -> CGPoint {
         CGPoint(x: lhs.x / rhs.width, y: lhs.y / rhs.height)
     }
-    
-    
+}
+
+extension CGSize {
+    // the center point of an area that is our size
+    var center: CGPoint {
+        CGPoint(x: width/2, y: height/2)
+    }
+    static func +(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
+    }
+    static func -(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.width - rhs.width, height: lhs.height - rhs.height)
+    }
+    static func *(lhs: Self, rhs: CGFloat) -> CGSize {
+        CGSize(width: lhs.width * rhs, height: lhs.height * rhs)
+    }
+    static func /(lhs: Self, rhs: CGFloat) -> CGSize {
+        CGSize(width: lhs.width / rhs, height: lhs.height / rhs)
+    }
 }
 
 extension Array where Element == NSItemProvider {
